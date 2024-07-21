ec60d40c1c31eef82f5ed70bab96e91630e513c3
Naina1005 <nainavincy@gmail.com>
2024-07-21 12:02:41 +0530

    Initial commit

---
+++ .vscode/basics.py
@@ -0,0 +1,12 @@
+#this file includes basic various trials and test in order to resolve doubts 
+a = 1234
+print(a)
+print("a")
+
+print(a , "a" , 12)
+
+b = input("enter a number")
+print(type(b))
+
+b = int(b)
+print(type(b))
---
+++ calculator.py
@@ -0,0 +1,14 @@
+#python program to build a calculator 
+
+#taking two numbers as input from user 
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : ")) 
+
+#logic for building a calculator 
+print("The addition of above numbers is : ", n1+n2)
+print("The subtraction of above numbers is : ", n1-n2)
+print("The multiplication of above numbers is : ", n1*n2)
+print("The division of above numbers is : ", n1/n2)
+print("The floor division of above numbers is : ", n1//n2)
+print("The modulus of above numbers is : ", n1%n2)
+print("The exponent of above numbers is : ", n1**n2)
